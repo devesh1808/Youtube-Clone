4337bc348403d5d60a731963facf99e953cabb12
devesh1808 <devesh1808@gmail.com>
2023-09-13 00:56:27 +0530

    added some more routes and added navbar component

--- src/App.js
+++ src/App.js
@@ -4,9 +4,12 @@ import { Box } from '@mui/material';
 const App = () => (
   <BrowserRouter>
     <Box sx={{ backgroundColor: '#000' }}>
-      Navbar
+      <Navbar />
       <Routes>
         <Route exact path='/' element={<Feed />} />
+        <Route path='/video/:id' element={<VideoDetail />} />
+        <Route path='/channel/:id' element={<ChannelDetail />} />
+        <Route path='/search/:searchTerm' element={<SearchFeed />} />
       </Routes>
     </Box>
   </BrowserRouter>
