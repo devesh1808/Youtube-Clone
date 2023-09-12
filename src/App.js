efd51f33964d78213181b9c61c5c64763ccad1db
devesh1808 <devesh1808@gmail.com>
2023-09-13 00:50:51 +0530

    did some imports and added routing in app.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
-import React from 'react';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Box } from '@mui/material';
 
-const App = () => {
-  return (
-    <div>App</div>
-  )
-}
+const App = () => (
+  <BrowserRouter>
+    <Box sx={{ backgroundColor: '#000' }}>
+      Navbar
+      <Routes>
+        <Route exact path='/' element={<Feed />} />
+      </Routes>
+    </Box>
+  </BrowserRouter>
+);
 
 export default App;
