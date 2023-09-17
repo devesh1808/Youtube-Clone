b25cd8d9287c4feb359e04cbdfedfd6bb2a52c61
devesh1808 <devesh1808@gmail.com>
2023-09-17 20:57:32 +0530

    created feed component for ui

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,11 +1,27 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { Box, Stack, Typography } from "@mui/material";
 
 const Feed = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <Stack sx={{ flexDirection: { sx: "column", md: "row" } }}>
+      <Box
+        sx={{
+          height: { sx: "auto", md: "92vh" },
+          borderRight: "1px solid #3d3d3d",
+          px: { sx: 0, md: 2 },
+        }}
+      >
+        Sidebar
+        <Typography
+          className="copyright"
+          variant="body2"
+          sx={{ mt: 1.5, color: "#fff" }}
+        >
+          Copyright © 2023 JSM Media
+        </Typography>
+      </Box>
+    </Stack>
+  );
+};
 
-export default Feed
+export default Feed;
